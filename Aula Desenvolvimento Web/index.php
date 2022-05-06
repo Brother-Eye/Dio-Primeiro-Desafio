1e723bd0e70f50f06b4389f7a7eda7aa7adce709
Diego Souza <diego.rosse.souza@gmail.com>
2022-05-06 10:55:47 -0300

    Novos Cursos

---
+++ Aula Desenvolvimento Web/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Meu primeiro site em PHP!</title>
+  </head>
+  <body>
+
+    <?php
+        for ($i=0; $i < 10; $i++) {
+          print("linha número ". $i . "<br />");
+        }
+     ?>
+  </body>
+</html>
